3a195254819aeb7247b0fb60d0891d66d543239a
naritayuuto <yuuto122narita@i.softbank.jp>
2022-12-05 22:46:40 +0900

    プレイヤーの動きのscript追加

---
+++ Assets/scripts/PlayerController.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] float _moveSpeed = 3;
+    [SerializeField] float m_turnSpeed = 3f;
+    Rigidbody _rb = default;
+    Animator anim = null;
+    /// <summary>入力された方向の XZ 平面でのベクトル</summary>
+    Vector3 _dir;
+    void Start()
+    {
+        _rb = GetComponent<Rigidbody>();
+        anim = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        // 方向の入力を取得し、方向を求める
+        float v = Input.GetAxisRaw("Vertical");
+        float h = Input.GetAxisRaw("Horizontal");
+
+        // 入力方向のベクトルを組み立てる
+        Vector3 dir = Vector3.forward * v + Vector3.right * h;
+
+        if (dir == Vector3.zero)
+        {
+            // 方向の入力がニュートラルの時は、y 軸方向の速度を保持するだけ
+            _rb.velocity = new Vector3(0f, _rb.velocity.y, 0f);
+        }
+        else
+        {
+            // カメラを基準に入力が上下=奥/手前, 左右=左右にキャラクターを向ける
+            dir = Camera.main.transform.TransformDirection(dir);    // メインカメラを基準に入力方向のベクトルを変換する
+            dir.y = 0;  // y 軸方向はゼロにして水平方向のベクトルにする
+
+            // 入力方向に滑らかに回転させる
+            Quaternion targetRotation = Quaternion.LookRotation(dir);
+            this.transform.rotation = Quaternion.Slerp(this.transform.rotation, targetRotation, Time.deltaTime * m_turnSpeed);  // Slerp を使うのがポイント
+
+            Vector3 velo = dir.normalized * _moveSpeed; // 入力した方向に移動する
+            velo.y = _rb.velocity.y;   // ジャンプした時の y 軸方向の速度を保持する
+            _rb.velocity = velo;   // 計算した速度ベクトルをセットする
+        }
+    }
+    void LateUpdate()
+    {
+        Vector3 velocity = _rb.velocity;
+        velocity.y = 0; // 上下方向の速度は無視する
+        anim.SetFloat("Speed", velocity.magnitude);
+    }
+}
