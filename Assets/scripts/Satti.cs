d09c0eee8c7a37b30366d5aaaa5c3f45358a6ecd
naritayuuto <yuuto122narita@i.softbank.jp>
2022-12-05 22:45:06 +0900

    判定内に目標が入ったら音楽を変えるscriptを追加

---
+++ Assets/scripts/Satti.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Satti : MonoBehaviour
+{
+
+    [SerializeField] private AudioSource satti;
+    [SerializeField] GameObject BGM;
+    private void Start()
+    {
+        satti = GetComponent<AudioSource>();
+        if(satti == null)
+        {
+            satti = BGM.GetComponent<AudioSource>();
+        }
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.CompareTag("Player"))
+        {
+            satti.Play();
+        }
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        if(other.CompareTag("Player"))
+        {
+            satti.Stop();
+        }
+    }
+}
