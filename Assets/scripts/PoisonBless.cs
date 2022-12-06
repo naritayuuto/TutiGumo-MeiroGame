d4b16c4edc753f2b85a6bae6bb19bfaac5f9dad3
naritayuuto <yuuto122narita@i.softbank.jp>
2022-12-06 14:30:20 +0900

    毒霧追加

---
+++ Assets/scripts/PoisonBless.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PoisonBless : MonoBehaviour
+{
+    [SerializeField] GameObject bless;
+    [SerializeField] Transform muzzle;
+    float count = 0;
+    float Descount = 1.8f;
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.CompareTag("Player"))
+        {
+            count += Time.deltaTime;
+            Instantiate(bless, muzzle.position, this.transform.rotation);
+            if(count >Descount)
+            {
+                Destroy(bless.gameObject);
+            }
+        }
+    }
+}
