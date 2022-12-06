4115d9f38f0930fcfa08046fcead816af751b410
naritayuuto <yuuto122narita@i.softbank.jp>
2022-12-06 14:29:02 +0900

    一部コメントアウト

---
+++ Assets/scripts/PoisonDead.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PoisonDead : MonoBehaviour
+{
+    SceneLoader _sceneLoader = null;
+    private void Start()
+    {
+        _sceneLoader = GameObject.FindGameObjectWithTag("SceneLoader").GetComponent<SceneLoader>();
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.CompareTag("Player"))
+        {
+            //Destroy(other.gameObject);
+            //プレイヤーのアニメーションを流す
+            _sceneLoader.PlayerPoisonDeadLoad();
+        }
+    }
+}
