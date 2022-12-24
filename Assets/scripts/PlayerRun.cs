e449c23e95e4f13ab2d6d5973039630852bd2123
naritayuuto <yuuto122narita@i.softbank.jp>
2022-12-24 12:09:45 +0900

    ファイル分け（BGMとSE）を行った。

---
+++ Assets/scripts/PlayerRun.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerRun : MonoBehaviour
+{
+    [SerializeField] float speed = 1f;
+    Rigidbody rb = default;
+    Animator anim = default;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        anim = GetComponent<Animator>();
+    }
+
+    void FixedUpdate()
+    {
+        this.transform.Translate(Vector3.forward * Time.deltaTime * speed); ;
+    }
+    void LateUpdate()
+    {
+        anim.SetFloat("Speed", 1.0f);
+    }
+}
