fc8532a231d50151e0d95a6bf33deb86e61505c9
naritayuuto <yuuto122narita@i.softbank.jp>
2023-03-03 17:54:56 +0900

    summaryをTooltipへ変更

--- Assets/scripts/WanderingSpider.cs
+++ Assets/scripts/WanderingSpider.cs
@@ -7,80 +7,80 @@ using UnityEngine.AI;
 public class WanderingSpider : MonoBehaviour
 {
     [SerializeField,Tooltip("目標との間隔、余裕を持った値にする")]
-    float dis = 5f;
+    float _dis = 5f;
     [SerializeField, Tooltip("Playerとの間隔、余裕を持った値にする")]
-    float playerFindDis = 5f;
+    float _playerFindDis = 5f;
     [SerializeField,Tooltip("プレイヤーを追跡する時間")]
-    float trackingTime = 30f;
-    float countTime = 0f;
-    /// <summary>徘徊場所をまとめた配列の要素数</summary>
-    int pointsNumber = 0;
-    /// <summary>0 == プレイヤー未発見、1 == プレイヤー発見</summary>
-    int mode;
-    [SerializeField,Tooltip("徘徊する場所")]
-    Vector3[] points;
-    GameObject player = null;
-    NavMeshAgent _agent;
-    MusicManager _musicM;
-    PlayerFind playerFind;
-    Animator _anim = default;
+    float _trackingTime = 30f;
+    float _countTime = 0f;
+    [Tooltip("_pointsの要素数")]
+    int _pointsNumber = 0;
+    [Tooltip("0 == プレイヤー未発見、1 == プレイヤー発見")]
+    int _mode;
     [Tooltip("playerを見つけたらTrue")]
     bool _playerPerception = false;
+    GameObject _player = null;
+    [SerializeField,Tooltip("徘徊する場所")]
+    Vector3[] _points;
+    NavMeshAgent _agent;
+    MusicManager _musicM;
+    PlayerFind _playerFind;
+    Animator _anim = null;
     // Start is called before the first frame update
     void Start()
     {
         _agent = GetComponent<NavMeshAgent>();
-        player = GameObject.Find("Player");
+        _player = GameManager.Instance.Player;
         _musicM = GameObject.FindGameObjectWithTag("MusicManager").GetComponent<MusicManager>();
         _anim = GetComponent<Animator>();
-        playerFind = GetComponentInChildren<PlayerFind>();
+        _playerFind = GetComponentInChildren<PlayerFind>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        Vector3 pos = points[pointsNumber];
-        if (player)
+        Vector3 pos = _points[_pointsNumber];
+        if (_player)
         {
-            float distance = Vector3.Distance(transform.position, player.transform.position);//自身とplayerの距離
-            if (distance > playerFindDis)//playerを見つけられる距離より、距離が離れていたら
+            float distance = Vector3.Distance(transform.position, _player.transform.position);//自身とplayerの距離
+            if (distance > _playerFindDis)//playerを見つけられる距離より、距離が離れていたら
             {
                 if(_playerPerception)//playerを見つけていたら
                 {
-                    countTime = 0;
+                    _countTime = 0;
                     _musicM.Bgm = MusicManager.BGM.Stage;
                     _musicM.PlayBGM(_musicM.Bgm);
                     _playerPerception = false;
                 }
-                mode = 0;
+                _mode = 0;
             }
             else
             {   
-                countTime += Time.deltaTime;
+                _countTime += Time.deltaTime;
                 if (_musicM.Bgm != MusicManager.BGM.playerPerception && _musicM.Bgm != MusicManager.BGM.PlayerFind)
                 {
                     _musicM.Bgm = MusicManager.BGM.playerPerception;
                     _musicM.PlayBGM(_musicM.Bgm);
                 }
-                mode = 1;
+                _mode = 1;
             }
         }
-        switch (mode)
+        switch (_mode)
         {
 
             case 0:
 
-                if (Vector3.Distance(transform.position, pos) < dis)
+                if (Vector3.Distance(transform.position, pos) < _dis)
                 {
-                    pointsNumber++;
-                    pointsNumber = pointsNumber % points.Length;
+                    _pointsNumber++;
+                    _pointsNumber = _pointsNumber % _points.Length;
                 }
                 _agent.SetDestination(pos);
                 break;
 
             case 1:
                     _playerPerception = true;
-                    _agent.destination = player.transform.position;//プレイヤーに向かって進む
+                    _agent.destination = _player.transform.position;//プレイヤーに向かって進む
                 break;
         }
     }
@@ -88,7 +88,7 @@ public class WanderingSpider : MonoBehaviour
     public void PlayerDead()
     {
         _playerPerception = false;
-        player = null;
+        _player = null;
     }
     private void LateUpdate()
     {
