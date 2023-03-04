424fe535a5f5817474bd37f5d92050b8a4c36efb
naritayuuto <yuuto122narita@i.softbank.jp>
2023-03-04 12:41:01 +0900

    mission（アイテムを一定数）達成にゴールへ簡単に向かえるように 難易度調整（下）。

--- Assets/scripts/MiniMap.cs
+++ Assets/scripts/MiniMap.cs
@@ -8,10 +8,20 @@ public class MiniMap : MonoBehaviour
     float _posY = 0;
     [SerializeField,Tooltip("プレイヤーオブジェクト"),Header("プレイヤーオブジェクト")]
     GameObject _player = null;
+    [SerializeField, Tooltip("ミニマップ表示用GameObject"), Header("ミニマップを表示しているオブジェクト")]
+    GameObject _miniMap = null;
 
+    private void Start()
+    {
+        _miniMap.SetActive(false);
+    }
     // Update is called once per frame
     void Update()
     {
+        if(GameManager.Instance.Item.ItemCount == 0)
+        {
+            _miniMap.SetActive(true);
+        }
         transform.position = new Vector3(_player.transform.position.x, _posY, _player.transform.position.z);
     }
 }
